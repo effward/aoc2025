3816f0420d6d1403eb182c0ca2c99a7773fd2711
effward <agf33@cornell.edu>
2025-12-02 12:00:24 -0800

    use raw strings for inputs

--- AoC25/AoC25.Tests/Inputs.cs
+++ AoC25/AoC25.Tests/Inputs.cs
@@ -4,15 +4,17 @@ public static class Inputs
 {
     public static int Day1Part1Output { get; set; } = 3;
     public static int Day1Part2Output { get; set; } = 6;
-    public static string Day1Input { get; set; } = @"
-L68
-L30
-R48
-L5
-R60
-L55
-L1
-L99
-R14
-L82";
+    public static string Day1Input { get; set; } = """
+
+                                                   L68
+                                                   L30
+                                                   R48
+                                                   L5
+                                                   R60
+                                                   L55
+                                                   L1
+                                                   L99
+                                                   R14
+                                                   L82
+                                                   """;
 }
--- AoC25/Program.cs
+++ AoC25/Program.cs
@@ -2,6 +2,19 @@
 
 using AoC25;
 
-Console.WriteLine("Day 1, Part 1: " + Day1.SolvePart1(""));
-Console.WriteLine("Day 1, Part 2: " + Day1.SolvePart2(""));
+const string day1Input = """
+                L68
+                L30
+                R48
+                L5
+                R60
+                L55
+                L1
+                L99
+                R14
+                L82
+                """;
+
+Console.WriteLine("Day 1, Part 1: " + Day1.SolvePart1(day1Input));
+Console.WriteLine("Day 1, Part 2: " + Day1.SolvePart2(day1Input));
 
