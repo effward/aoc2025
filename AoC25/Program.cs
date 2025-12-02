41de42514cc6c471897625b175ebc4e1faab539f
effward <agf33@cornell.edu>
2025-12-02 11:56:44 -0800

    Move Day1 inputs to test project

--- AoC25/Program.cs
+++ AoC25/Program.cs
@@ -2,6 +2,6 @@
 
 using AoC25;
 
-Console.WriteLine("Day 1, Part 1: " + Day1.SolvePart1());
-Console.WriteLine("Day 1, Part 2: " + Day1.SolvePart2());
+Console.WriteLine("Day 1, Part 1: " + Day1.SolvePart1(""));
+Console.WriteLine("Day 1, Part 2: " + Day1.SolvePart2(""));
 
