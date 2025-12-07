db45b1f7768734e5738523b6dfb8a642af9fca43
effward <agf33@cornell.edu>
2025-12-07 11:31:44 -0800

    Minor cleanup

--- AoC25/Day7.cs
+++ AoC25/Day7.cs
@@ -4,25 +4,125 @@ public class Day7() : DayBase(7)
 {
     public override long SolvePart2(string input)
     {
-        if (_RootNode is null)
-        {
-            throw new Exception("You need to run part 1 before part 2");
-        }
-
         return _Sum;
     }
 
     private static long _Sum = 0;
-    private static Node? _RootNode = null;
     private static Dictionary<Position, Node> _NodePositions = new ();
-    private record struct Position(int Row, int Col);
 
-    private class Node(Position position, ISet<Node> parents, ISet<Node> children, long count)
+    public override long SolvePart1(string input)
     {
-        public Position Position { get; } = position;
-        public ISet<Node> Parents { get; } = parents;
-        public ISet<Node> Children { get; } = children;
-        public long Count { get; set; } = count;
+        var grid = BuildGrid(input);
+        
+        var nodes = new List<Node>();
+        var nextNodes = new List<Node>
+        {
+            FindRootNode(grid)
+        };
+        
+        var totalSplits = 0;
+        while (nextNodes.Count > 0)
+        {
+            nodes = nextNodes;
+            nextNodes = [];
+            foreach (var node in nodes)
+            {
+                ProcessNode(node);
+            }
+        }
+        
+        var sum = nodes.Sum(x => x.Count);
+        _Sum = sum;
+        
+        return totalSplits;
+
+        void ProcessNode(Node node)
+        {
+            var nodePosition = node.Position;
+            nodePosition.Row++;
+            grid[nodePosition.Row][nodePosition.Col] = '|';
+            nodePosition.Row++;
+
+            if (nodePosition.Row >= grid.Length)
+            {
+                return;
+            }
+                
+            var curr = grid[nodePosition.Row][nodePosition.Col];
+            if (curr == '^')
+            {
+                var leftNode = BuildNode(nodePosition.Row, nodePosition.Col - 1, node);
+                var rightNode = BuildNode(nodePosition.Row, nodePosition.Col + 1, node);
+
+                if (leftNode is not null || rightNode is not null)
+                {
+                    totalSplits++;
+                }
+            }
+            else if (curr == '.')
+            {
+                var nextNode = BuildNode(nodePosition.Row, nodePosition.Col, node);
+                if (nextNode is null)
+                {
+                    totalSplits--;
+                }
+                grid[nodePosition.Row][nodePosition.Col] = '|';
+            }
+            else if (curr == '|')
+            {
+                var existingNode = _NodePositions[nodePosition];
+                existingNode.Count += node.Count;
+            }
+            else
+            {
+                throw new ArgumentException("Invalid input, unexpected char: " + curr);
+            }
+        }
+
+        Node? BuildNode(int row, int col, Node parent)
+        {
+            var node = NewNode(row, col, parent.Count);
+            if (node is null)
+            {
+                var position = new Position(row, col);
+                var existingNode = _NodePositions[position];
+                existingNode.Parents.Add(parent);
+                existingNode.Count += parent.Count;
+                parent.Children.Add(existingNode);
+                
+                return null;
+            }
+
+            node.Parents.Add(parent);
+            parent.Children.Add(node);
+            
+            grid[node.Position.Row][node.Position.Col] = '|';
+            nextNodes.Add(node);
+
+            return node;
+        }
+    }
+
+    private static Node FindRootNode(char[][] grid)
+    {
+        Node? rootNode = null;
+
+        for (var i = 0; i < grid[0].Length; i++)
+        {
+            if (grid[0][i] == 'S')
+            {
+                rootNode = NewNode(0, i, 1) ?? throw new Exception("Graph should be empty when root node created");
+                break;
+            }
+        }
+
+        if (rootNode is null)
+        {
+            PrintGrid(grid);
+            throw new ArgumentException("Invalid input");
+        }
+        
+        return rootNode;
     }
     
     private static Node? NewNode(int row, int col, long count)
@@ -37,144 +137,15 @@ public class Day7() : DayBase(7)
 
         return node;
     }
-
-    public override long SolvePart1(string input)
+    
+    private record struct Position(int Row, int Col);
+    
+    private class Node(Position position, ISet<Node> parents, ISet<Node> children, long count)
     {
-        var grid = BuildGrid(input);
-        // PrintGrid(grid);
-
-        var curRow = 0;
-        var curCol = -1;
-        Node rootNode = default;
-
-        for (var i = 0; i < grid[0].Length; i++)
-        {
-            if (grid[0][i] == 'S')
-            {
-                curCol = i;
-                rootNode = NewNode(0, curCol, 1) ?? throw new Exception("Graph should be empty when root node created");
-                _RootNode = rootNode;
-                break;
-            }
-        }
-
-        if (curCol == -1 || rootNode == default)
-        {
-            PrintGrid(grid);
-            throw new ArgumentException("Invalid input");
-        }
-
-        var nodes = new List<Node>
-        {
-            rootNode
-        };
-        var nextNodes = new List<Node>();
-        var totalSplits = 0;
-        while (curRow < grid.Length)
-        {
-            nextNodes = [];
-            foreach (var node in nodes)
-            {
-                var nodePosition = node.Position;
-                nodePosition.Row++;
-                grid[nodePosition.Row][nodePosition.Col] = '|';
-                nodePosition.Row++;
-
-                if (nodePosition.Row >= grid.Length)
-                {
-                    continue;
-                }
-                
-                var curr = grid[nodePosition.Row][nodePosition.Col];
-                if (curr == '^')
-                {
-                    var leftNode = BuildNode(nodePosition.Row, nodePosition.Col - 1, node);
-                    var rightNode = BuildNode(nodePosition.Row, nodePosition.Col + 1, node);
-
-                    if (leftNode is not null || rightNode is not null)
-                    {
-                        totalSplits++;
-                    }
-                }
-                else if (curr == '.')
-                {
-                    var nextNode = BuildNode(nodePosition.Row, nodePosition.Col, node);
-                    if (nextNode is null)
-                    {
-                        totalSplits--;
-                    }
-                    grid[nodePosition.Row][nodePosition.Col] = '|';
-                }
-                else if (curr == '|')
-                {
-                    var existingNode = _NodePositions[nodePosition];
-                    existingNode.Count += node.Count;
-                }
-                else
-                {
-                    throw new ArgumentException("Invalid input, unexpected char: " + curr);
-                }
-            }
-
-            if (nextNodes.Count == 0)
-            {
-                break;
-            }
-            
-            nodes = nextNodes;
-            curRow += 2;
-        }
-        
-        var n = string.Join(", ", nodes.Select(x => x.Count.ToString()));
-        Console.WriteLine($"Nodes: [{n}]");
-        var sum = nodes.Sum(x => x.Count);
-        Console.WriteLine($"Sum: {sum}");
-        _Sum = sum;
-        
-        PrintGrid(grid);
-        return totalSplits;
-
-        Node? BuildNode(int row, int col, Node parent)
-        {
-            var node = NewNode(row, col, parent.Count);
-            if (node is null)
-            {
-                var position = new Position(row, col);
-                if (!_NodePositions.TryGetValue(position, out var existingNode))
-                {
-                    throw new Exception(
-                        "Node just couldn't be created because of an existing node at this position, but now that can't be found");
-                }
-
-                if (!existingNode.Parents.Add(parent))
-                {
-                    throw new Exception("Existing node already has this parent");
-                }
-                
-                existingNode.Count += parent.Count;
-
-                if (!parent.Children.Add(existingNode))
-                {
-                    throw new Exception("Parent already has existing Node as child");
-                }
-                
-                return null;
-            }
-
-            if (!node.Parents.Add(parent))
-            {
-                throw new ArgumentException("Parent already exists on this brand new node");
-            }
-            if (!parent.Children.Add(node))
-            {
-                // TODO: Is this a problem?
-                throw new ArgumentException("Invalid input, this node is already a child on this parent");
-            }
-            
-            grid[node.Position.Row][node.Position.Col] = '|';
-            nextNodes.Add(node);
-
-            return node;
-        }
+        public Position Position { get; } = position;
+        public ISet<Node> Parents { get; } = parents;
+        public ISet<Node> Children { get; } = children;
+        public long Count { get; set; } = count;
     }
+    
 }
